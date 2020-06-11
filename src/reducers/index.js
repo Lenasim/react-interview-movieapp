852601a4649e3debad51eab010707649a116b104
LenaSim <radical.rin@gmail.com>
2020-06-11 20:03:14 +0200

    arrange reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,39 +3,78 @@ import { SET_CATEGORY, SET_MOVIES, LIKE, DISLIKE } from '../actions';
 const initialState = {
   movies: [],
   selectedCat: [],
-  categories: []
+  categories: [],
+  likes: [],
+  dislikes: []
+}
+
+const handleLikeVote = (state, action) => {
+  let likes = [...state.likes]
+  let dislikes = [...state.dislikes]
+  if (likes.indexOf(action.movieId) === -1) {
+    likes.push(action.movieId)
+  }
+  if (dislikes.indexOf(action.movieId) > -1) {
+    dislikes.splice(dislikes.indexOf(action.movieId), 1);
+  }
+
+  return Object.assign({}, state, {
+    likes: likes,
+    dislikes: dislikes,
+    movies: state.movies.map(m => {
+      if (m.id !== action.movieId) {
+        return m;
+      }
+      return Object.assign({}, m, {
+        isLiked: true,
+        likes: (m.isLiked === true || state.likes.some(s => s === m.id)) ? m.likes : m.likes + 1,
+        dislikes: (m.isLiked === false && state.dislikes.some(s => s === m.id)) ? m.dislikes - 1 : m.dislikes
+      })
+    })
+  })
+}
+
+const handleDislikeVote = (state, action) => {
+  let dislikes = [...state.dislikes]
+  let likes = [...state.likes]
+  if (dislikes.indexOf(action.movieId) === -1) {
+    dislikes.push(action.movieId)
+  }
+  if (likes.indexOf(action.movieId) > -1) {
+    likes.splice(likes.indexOf(action.movieId), 1);
+  }
+
+  return Object.assign({}, state, {
+    likes: likes,
+    dislikes: dislikes,
+    movies: state.movies.map(m => {
+      if (m.id !== action.movieId) {
+        return m;
+      }
+      return Object.assign({}, m, {
+        isLiked: false,
+        likes: (m.isLiked === true && state.likes.some(s => s === m.id)) ? m.likes - 1 : m.likes,
+        dislikes: (m.isLiked === false || state.dislikes.some(s => s === m.id)) ? m.dislikes : m.dislikes + 1
+      })
+    })
+  })
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_MOVIES:
-      return { ...state, movies: action.movies, categories: action.movies.map(i => i.category).filter((value, index, self) => self.indexOf(value) === index) }
+      return {
+        ...state,
+        movies: action.movies,
+        categories: action.movies.map(i => i.category)
+          .filter((value, index, self) => self.indexOf(value) === index)
+      }
     case SET_CATEGORY:
       return Object.assign({}, state, { selectedCat: action.selectedCat })
     case LIKE:
-      return Object.assign({}, state, {
-        movies: state.movies.map(m => {
-          if (m.id !== action.movieId) {
-            return m;
-          }
-          return Object.assign({}, m, {
-            isLiked: true,
-            likes: m.likes + 1
-          })
-        })
-      })
+      return handleLikeVote(state, action)
     case DISLIKE:
-      return Object.assign({}, state, {
-        movies: state.movies.map(m => {
-          if (m.id !== action.movieId) {
-            return m;
-          }
-          return Object.assign({}, m, {
-            isLiked: false,
-            dislikes: m.dislikes + 1
-          })
-        })
-      })
+      return handleDislikeVote(state, action)
     default:
       return state
   }
