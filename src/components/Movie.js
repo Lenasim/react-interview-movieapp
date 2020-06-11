909138092b805c199318904eb3300e8383555a26
LenaSim <radical.rin@gmail.com>
2020-06-11 20:04:59 +0200

    Styling movie card view

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   return (
     <div className="card">
       <h4 className="card-heading">{props.title}</h4>
-      <p variant="primary">Category</p>
+      <p className="card-cat">Category</p>
       <p className="card-text"> {props.category}</p>
 
       <div className="container">
@@ -13,19 +13,17 @@ const Movie = (props) => {
           id={props.id}
           icon="fa-thumbs-up"
           counter={props.likes}
-          isLiked={}
+          isActive={props.isLiked === true}
           handleClick={props.handleLike}
         />
-
-        <button id={props.id} onClick={props.handleLike} className="likebutton" >
-          <i className={`fa fa-thumbs-up like ${props.isLiked === true ? 'active' : null}`} aria-hidden="true"></i>
-        </button>
-        {props.likes}
         {' '}
-        <button id={props.id} onClick={props.handleDislike} className="likebutton">
-          <i className={`fa fa-thumbs-down dislike ${props.isLiked === false ? 'active' : null}`} aria-hidden="true"></i>
-        </button>
-        {props.dislikes}
+        <LikeButton
+          id={props.id}
+          icon="fa-thumbs-down"
+          counter={props.dislikes}
+          isActive={props.isLiked === false}
+          handleClick={props.handleDislike}
+        />
       </div>
     </div>
   )
